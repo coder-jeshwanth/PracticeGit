1a72b1e60136fdb2fe1b4eff667e5cc4525fa345
coder-jeshwanth <24mcaa32@kristujayanti.com>
2025-07-08 23:05:19 +0530

    commited

--- Demo.java
+++ Demo.java
@@ -4,21 +4,14 @@ public class Demo {
         System.out.println("This is the develop branch");
     }
 
-
-<<<<<<< HEAD
-=======
     void jeshwanth(){
-
         System.out.println("This is Jeshwanth");
 
->>>>>>> 5039c7fda9c88f93fd6810c8ab7452fdb0b1be77
     void don(){
-
         System.out.println("This is Don");
 
 
     void krishna(){
-
        System.out.println("This is Krishna");
 
     }
